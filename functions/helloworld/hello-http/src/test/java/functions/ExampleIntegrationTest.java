08e4c781a5a37ce797a4f5a7b6fa962c0e59085f
ace-n <anassri@google.com>
2020-04-22 17:05:21 -0700

    BUGFIX remove -nsu switch

--- functions/helloworld/hello-http/src/test/java/functions/ExampleIntegrationTest.java
+++ functions/helloworld/hello-http/src/test/java/functions/ExampleIntegrationTest.java
@@ -61,7 +61,7 @@ public class ExampleIntegrationTest {
 
     // Emulate the function locally by running the Functions Framework Maven plugin
     emulatorProcess = new ProcessBuilder()
-        .command("mvn", "-nsu", "function:run")
+        .command("mvn", "function:run")
         .directory(new File(baseDir))
         .start();
   }
